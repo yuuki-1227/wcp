7796375ac77053811928f08300707362feaf407d
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-07-24 12:06:40 +0900

    [chapter8]for01

--- JavaWcp/src/DoWhile01.java
+++ JavaWcp/src/DoWhile01.java
@@ -2,7 +2,7 @@
 public class DoWhile01 {
 	public static void main(String[] args) {
 		
-		int number = 1;
+		int number = 100;
 		do {
 			number *= 2;
 			System.out.println("DoWhile01 = " + number);
