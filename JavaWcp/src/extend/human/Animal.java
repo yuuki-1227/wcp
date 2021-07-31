477adc12a840986c4d36e9052465da358e5907cd
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-07-31 18:15:38 +0900

    [chapter13]animal

---
+++ JavaWcp/src/extend/human/Animal.java
@@ -0,0 +1,12 @@
+package extend.human;
+
+public abstract class Animal {
+	protected String name;
+	
+	public void eat() {
+		System.out.println(name + "が食事をしました");
+	}
+	
+	public abstract void cry();
+
+}
