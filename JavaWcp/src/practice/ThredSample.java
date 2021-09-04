3454f91630c337a04f5386a7b132706fab1b689d
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-09-04 20:03:07 +0900

    [chapter15_4_sample]

---
+++ JavaWcp/src/practice/ThredSample.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class ThreadSample implements Runnable {
+    int num;
+
+    // コンストラクタ
+    public ThreadSample(int num) {
+        this.num = num;
+    }
+
+    // runメソッドを作成
+    // Chapter15_4と同じようにループごとに1秒間スリープして表示
+    ・・・
+}
