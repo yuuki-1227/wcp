baf336981a6f867b12a88d41463906f3a56c0e1d
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-09-12 11:40:55 +0900

    [chapter15_3_answer]

--- JavaWcp/src/practice/Chapter15_3.java
+++ JavaWcp/src/practice/Chapter15_3.java
@@ -2,21 +2,26 @@ package practice;
 
 public class Chapter15_3 {
     public static void main(String[] args) {
-            int i1 = 123;
-            double d1 = i1;
-            System.out.println(d1);
 
-            double d2 = 12.345;
-            int i2 = d2;
-            System.out.println(i2);
+        int i1 = 123;
+        // 暗黙的にキャストされる
+        double d1 = i1;
+        System.out.println(d1);
 
-            int i3 = 1;
-            int i4 = 2;
-            double d3 = i3 / i4;
-            System.out.println(d3);
+        double d2 = 12.345;
+        // 明示的にキャストしないとコンパイルエラー
+        int i2 = (int) d2;
+        System.out.println(i2);
 
-            int i5 = 128;
-            byte b1 = i5;
-            System.out.println(b1);
+        int i3 = 1;
+        int i4 = 2;
+        // キャストしないと期待の結果にならない
+        double d3 = (double) i3 / i4;
+        System.out.println(d3);
+
+        int i5 = 128;
+        // 明示的にキャストしないとコンパイルエラー
+        byte b1 = (byte) i5;
+        System.out.println(b1);
     }
 }
